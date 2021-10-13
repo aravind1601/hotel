03564786f6cb60d4d5917ac6ca6bfc64f78bc121
aravind1601 <dineshoptimist93>
2021-10-13 14:41:43 +0530

    my first array

---
+++ src/org/test/Data.java
@@ -0,0 +1,27 @@
+package org.test;
+//array
+public class Data {
+	public static void main(String[] args) {
+		int v[]=new int[6];
+		v[0]=27;
+		v[1]=37;
+		v[2]=47;
+		v[3]=47;
+		v[4]=57;
+		v[5]=67;
+		
+		for (int i = 0; i < 6; i++) {
+			System.out.println(v[i]);
+			
+			
+			}
+			
+		
+	for (int i : v) {
+			System.out.println(i);
+	}
+		
+		
+	}
+
+}
